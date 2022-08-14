f4576ffaa5028700f604c284a4ca67324bbbda5d
Alexander Pelta <alexpxrad@gmail.com>
2022-08-13 23:19:39 -0400

    more problems

---
+++ Aug13/makeNegative.js
@@ -0,0 +1,12 @@
+// In this simple assignment you are given a 
+// number and have to make it negative. 
+// But maybe the number is already negative?
+
+function makeNegative(num) {
+    if (num > 0 ){
+      return -Math.abs(num)
+    }
+      else {
+        return num
+      }
+    }
